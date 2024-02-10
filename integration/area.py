29f374d6dec7e5c61f91272c0bd585e3b9b67bba
Akash <akashbalan08@gmail.com>
2024-02-09 19:57:50 -0500

    Added assignment 2 files

---
+++ integration/area.py
@@ -0,0 +1,8 @@
+def areaofsquare(side1, side2):
+    return side1 * side2
+
+
+def areaofcircle(radius):
+    return 3.14 * radius * radius
+
+
---
+++ integration/area_test.py
@@ -0,0 +1,9 @@
+import area
+
+
+def test_area_square():
+    assert area.areaofsquare(2, 3) == 6
+
+
+def test_area_circle():
+    assert area.areaofcircle(2) == 12.56
